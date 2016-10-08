a23fa8c249f49ba630d7e0dd38403730452be5e6
joelam <joelam789@hotmail.com>
2016-10-08 19:03:30 +0800

    rename some properties of the player

--- LiveStreamingClient/stream-player.js
+++ LiveStreamingClient/stream-player.js
@@ -1,8 +1,8 @@
 
 	function SimpleWebSocketStreamPlayer (videoOptions, audioOptions) {
 	
-		this.videoPlayer = new SimpleWebVideoStreamPlayer(videoOptions);
-		this.audioPlayer = (audioOptions == undefined || audioOptions == null) ? null : new SimpleWebAudioStreamPlayer(audioOptions);
+		this.video = new SimpleWebVideoStreamPlayer(videoOptions);
+		this.audio = (audioOptions == undefined || audioOptions == null) ? null : new SimpleWebAudioStreamPlayer(audioOptions);
 		
 		this.url = "";
 		
@@ -15,15 +15,22 @@
 		this.audioFilter = null;
 		this.videoContainer = null;
 		
+		this.volume = function(value) {
+			if (this.audio != null) {
+				return this.audio.volume(value);
+			}
+			return 0;
+		};
+		
 		this.close = function() {
-			this.videoPlayer.enabled = false;
-			if (this.audioPlayer != null) this.audioPlayer.enabled = false;
+			this.video.enabled = false;
+			if (this.audio != null) this.audio.enabled = false;
 			if (this.socket != null) {
 				this.socket.close()
 				this.socket = null;
 			}
-			this.videoPlayer.clear();
-			if (this.audioPlayer != null) this.audioPlayer.clear();
+			this.video.clear();
+			if (this.audio != null) this.audio.clear();
 		};
 		
 		this.open = function(url) {
@@ -38,9 +45,9 @@
 				if (typeof data != "string") {
 					var binArr = new Uint8Array(data);
 					if (binArr[0] == 0 && binArr[1] == 0 && binArr[2] == 0 && binArr[3] == 1) {
-						this.videoPlayer.decode(data);
+						this.video.decode(data);
 					} else {
-						if (this.audioPlayer != null) this.audioPlayer.decode(data);
+						if (this.audio != null) this.audio.decode(data);
 					}
 				} else {
 					this.mediaInfo = data;
@@ -54,54 +61,54 @@
 					}
 					this.videoInfo = vinfopart;
 					console.log("video info: " + vinfopart);
-					if (this.videoPlayer.domNode != null && this.videoPlayer.domNode.parentNode != null) {
-						this.videoPlayer.updateMediaInfo(vinfopart);
+					if (this.video.domNode != null && this.video.domNode.parentNode != null) {
+						this.video.updateMediaInfo(vinfopart);
 					}
 					this.audioInfo = ainfopart;
 					console.log("audio info: " + ainfopart);
-					if (this.audioPlayer != null) this.audioPlayer.updateMediaInfo(ainfopart);
+					if (this.audio != null) this.audio.updateMediaInfo(ainfopart);
 				}
 			}.bind(this);
 			this.socket.onopen = function() {
-				if (this.audioPlayer != null) this.audioPlayer.open(this.audioFilter);
-				if (this.videoContainer != null) this.videoContainer.appendChild(this.videoPlayer.domNode);
+				if (this.audio != null) this.audio.open(this.audioFilter);
+				if (this.videoContainer != null) this.videoContainer.appendChild(this.video.domNode);
 				if (this.onOpen != undefined && this.onOpen != null) this.onOpen();
 			}.bind(this);
 			this.socket.onclose = function() {
-				if (this.audioPlayer != null) this.audioPlayer.close();
+				if (this.audio != null) this.audio.close();
 				if (this.onClose != undefined && this.onClose != null) this.onClose();
 			}.bind(this);
 			
-			this.videoPlayer.enabled = true;
+			this.video.enabled = true;
 		};
 		
-		this.pause = function() {
+		this.disable = function() {
 			if (this.socket == null || this.socket.readyState != 1) return;
-			this.videoPlayer.enabled = false;
-			if (this.audioPlayer != null) this.audioPlayer.enabled = false;
+			this.video.enabled = false;
+			if (this.audio != null) this.audio.enabled = false;
 		};
 		
-		this.resume = function() {
+		this.enable = function() {
 			if (this.socket == null || this.socket.readyState != 1) return;
-			this.videoPlayer.enabled = true;
-			if (this.audioPlayer != null) this.audioPlayer.enabled = true;
+			this.video.enabled = true;
+			if (this.audio != null) this.audio.enabled = true;
 		};
 		
 		this.isPlaying = function() {
 			if (this.socket == null || this.socket.readyState != 1) return false;
-			if (this.videoPlayer.enabled) return true;
-			if (this.audioPlayer != null && this.audioPlayer.enabled) return true;
+			if (this.video.enabled) return true;
+			if (this.audio != null && this.audio.enabled) return true;
 			return false;
 		};
 		
 		this.playDummySound = function() {
-			if (this.audioPlayer != null) {
-				if (!this.audioPlayer.isPlayingDummy) this.audioPlayer.playDummy();
+			if (this.audio != null) {
+				if (!this.audio.isPlayingDummy) this.audio.playDummy();
 			}
 		};
 		
-		this.videoPlayer.onRenderFirstFrameComplete = function () {
-			if (this.audioPlayer != null) this.audioPlayer.enabled = true;
+		this.video.onRenderFirstFrameComplete = function () {
+			if (this.audio != null) this.audio.enabled = true;
 			if (this.onPlay != undefined && this.onPlay != null) this.onPlay();
 		}.bind(this);
 		
